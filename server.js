0cbae3b759c7d113e8737f5df357f7a7b2a53a31
harshita98-web <62091091+harshita98-web@users.noreply.github.com>
2020-09-13 18:23:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(__dirname + '/dist/ecom-project'));
 
   app.use(cors());
   
-const connection = mysql.createPool({
+const pool = mysql.createPool({
     connectionLimit : 100,
     host: '127.0.0.1',
     user: 'root',
@@ -16,64 +16,96 @@ const connection = mysql.createPool({
     database: 'heroku_c0acbdb4873c8f2'
   });
 
-  app.get('/api/productslist' , (req,resp) => {
-       connection.query(`Select * from category`,(error,rows,fields) => {
-        if(error){
-            console.log('error in query',error)
+  
+app.get('/api/productslist', (req, resp) => {
+  pool.getConnection(function (err, connection) {
+    if (err) {
+      connection.release();
+      console.log('error occures solve it please');
+    }
+    else {
+      connection.query(`Select * from category`, (error, rows, fields) => {
+        if (error) {
+          console.log('error in query', error)
         }
-        else{
-            resp.send(rows);
+        else {
+          resp.send(rows);
         }
-       })
-  });
-
-  app.get('/api/categorywise/:id' , (req,resp) => {
-    let id = parseInt(req.params.id);
-    // resp.send(name);
-    connection.query(`Select * from products WHERE id = ${id}`,(error,rows,fields) => {
-     if(error){
-         console.log('error in query',error)
-     }
-     else{
-         resp.send(rows);
-     }
-    })
-});
-
-  app.get('/api/products' , (req,resp) => {
-    connection.query(`Select * from product_listing`,(error,rows,fields) => {
-     if(error){
-         console.log('error in query',error)
-     }
-     else{
-         resp.send(rows);
-     }
-    })
-});
-
-app.get('/api/subcategory/:catid' , (req,resp) => {
-  let catid = parseInt(req.params.catid);
-  connection.query(`Select * from sub_category WHERE category_id = ${catid}`,(error,rows,fields) => {
-   if(error){
-       console.log('error in query',error)
-   }
-   else{
-       resp.send(rows);
-   }
+      })
+    }
   })
 });
 
-  app.post('/api/login' , (req, res) => {
-    const token = jwt.sign({user : '1' , name : 'harshita'} , 'my_secret_key');
-    res.json({
-      token : token
-    });
-  });
+app.get('/api/categorywise/:id', (req, resp) => {
+  let id = parseInt(req.params.id);
+  // resp.send(name);
+  pool.getConnection(function (err, connection) {
+    if (err) {
+      connection.release();
+      console.log('error occures solve it please');
+    }
+    else {
+      connection.query(`Select * from products WHERE id = ${id}`, (error, rows, fields) => {
+        if (error) {
+          console.log('error in query', error)
+        }
+        else {
+          resp.send(rows);
+        }
+      })
+    }
+  })
+});
 
-  // function verifytoken(){
-  //   jwt.verify
-  // }
+app.get('/api/products', (req, resp) => {
+  pool.getConnection(function (err, connection) {
+    if (err) {
+      connection.release();
+      console.log('error occures solve it please');
+    }
+    else {
+      connection.query(`Select * from product_listing`, (error, rows, fields) => {
+        if (error) {
+          console.log('error in query', error)
+        }
+        else {
+          resp.send(rows);
+        }
+      })
+    }
+  })
+});
 
+app.get('/api/subcategory/:catid', (req, resp) => {
+  let catid = parseInt(req.params.catid);
+  pool.getConnection(function (err, connection) {
+    if (err) {
+      connection.release();
+      console.log('error occures solve it please');
+    }
+    else {
+      connection.query(`Select * from sub_category WHERE category_id = ${catid}`, (error, rows, fields) => {
+        if (error) {
+          console.log('error in query', error)
+        }
+        else {
+          resp.send(rows);
+        }
+      })
+    }
+  })
+});
+
+// app.post('/api/login', (req, res) => {
+//   const token = jwt.sign({ user: '1', name: 'harshita' }, 'my_secret_key');
+//   res.json({
+//     token: token
+//   });
+// });
+
+// function verifytoken(){
+//   jwt.verify
+// }
 
 
 //   app.listen(3000, () => {
